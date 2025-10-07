3d1a1a25088d3d775531d65eef19b6536befd214
Arik Chakma <arikchangma@gmail.com>
2025-10-08 00:03:26 +0600

    feat: index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { Route } from './+types/_index';
+import { geolocation } from '~/lib/geo';
+import { resolveTimezone, toTimezoneTime } from '~/lib/time';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   const title = 'TZD — Share Your Time Across Time Zones';
@@ -8,11 +12,10 @@ export function meta({}: Route.MetaArgs) {
     'https://tzd.fyi/api/og?from=America/New_York&to=Asia/Dhaka&fromTime=10:00&toTime=20:00&diffInHours=10';
 
   return [
-    { title: 'TZD — Share Your Time Across Time Zones' },
+    { title },
     {
       name: 'description',
-      content:
-        'Create short, shareable links for local times. Instantly see what your time means in another person’s timezone — perfect for meetings, events, and remote work.',
+      content: description,
     },
     {
       name: 'keywords',
@@ -86,8 +89,49 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export function loader(args: Route.LoaderArgs) {
+  const { countryCode, city } = geolocation(args.request.headers);
+  const tz = resolveTimezone(countryCode, city) || 'UTC';
+  const now = toTimezoneTime(new Date(), tz);
+
+  return { tz, now };
+}
+
+export default function Home(props: Route.ComponentProps) {
+  const { tz, now } = props.loaderData;
+  const [region, city] = tz.split('/');
+
   return (
-    <div className="flex h-screen items-center justify-center">Hello World</div>
+    <div className="mx-auto max-w-xl p-10 max-sm:px-5">
+      <h1 className="text-4xl font-bold text-balance max-sm:text-3xl">
+        Share Your Time Across Time Zones
+      </h1>
+      <p className="mt-5 text-lg text-balance text-zinc-600">
+        Turn any time into a simple link. Send it to anyone, anywhere — and
+        they’ll instantly see what that time means in their timezone.
+      </p>
+
+      <div className="mt-14 flex flex-col gap-4">
+        <p className="text-lg text-zinc-600">See how it works:</p>
+        <Link
+          to={`/${region}/${city}/${now}`}
+          className="text-2xl font-medium tracking-wide break-words text-rose-500 underline underline-offset-3 hover:text-rose-600"
+        >
+          https://tzd.fyi/{region}/{city}/{now}&ensp;
+          <span className="text-3xl">↗</span>
+        </Link>
+        <p className="text-lg text-balance text-zinc-600">
+          Send this link and others will see what <b>{now}</b> in <b>{city}</b>{' '}
+          means for them.
+        </p>
+      </div>
+    </div>
   );
+}
+
+export function headers(args: Route.HeadersArgs) {
+  return {
+    'Cache-Control': 'public, max-age=60, stale-while-revalidate=600',
+    Vary: 'X-Vercel-IP-Country, X-Vercel-IP-City',
+  };
 }
